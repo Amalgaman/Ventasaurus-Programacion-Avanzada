a3bd1e8bb4421d7c72f4c0ac6608486fbf3656d4
IgnacioPat <ignacio.patafio@davinci.edu.ar>
2023-06-07 17:59:40 -0300

    algo +
    ponele

--- SistemaVentasaurus/src/Interfaz/MenuCliente.java
+++ SistemaVentasaurus/src/Interfaz/MenuCliente.java
@@ -23,81 +23,99 @@ public class MenuCliente {
 	// 1 info
 	// 0 cruz
 
-	/*
-	 * public static void clienteVerMenu() { String[] opciones = { "Ver Conciertos",
-	 * "Comprar entradas", "Solicitar devolucion", "Salir" }; int xd; do { xd =
-	 * JOptionPane.showOptionDialog(null, "Seleccione una de las opciones",
-	 * "Ventasaurus - Bienvenido !", 0, 0, null, opciones, 0); switch (xd) { case 0:
-	 * listaConciertosCliente(); break; case 1: // CompraryPagar(); break; case 2:
-	 * SolicitudDeDevolucion(); break; case 3: JOptionPane.showMessageDialog(null,
-	 * "Abuenoadiosmaster"); break; default: break; } } while (xd != 3);// repetir
-	 * hasta que le de salir // listaConciertos(); }
-	 */
+	static Verifica verifica = new Verifica();
+	
+	Conexion con = new Conexion();
+
+	Connection conexion = (Connection) con.conectar();
+
+	PreparedStatement stmt;
+
 	public static void listaConciertosCliente() {
+		//no va
+
+		int i=0;
+		LinkedList<Concierto> listaTraida = verifica.verificaListaConciertos();
+		
+		String[] conciertoLista = new String[listaTraida.size()+1];
+		i=1;		
+		for (Concierto concierto : listaTraida) {
+			conciertoLista[i] = concierto.getNombre();
+			i++;
+		}
+
 		String opConcierto;
-		ImageIcon icon = new ImageIcon("src/img/tickets.png");	
+		ImageIcon icon = new ImageIcon("src/img/tickets.png");
 		int op;
 		String[] opcClienteConc = { "Comprar entradas", "Volver" };
-		String[] opcionesConcierto = { "Los Redondos 03/05/2023", "SodaStereo 08/05/2023",
-				"Las Pastillas del Abuelo 08/10/2023", "Hermetica 12/05/2023", "Fito Paez 13/07/2023",
-				"Leon Gieco 08/05/2023", "Sumo 24/10/2023" };
+	
+	//	String[] opcionesConcierto = { "Los Redondos 03/05/2023", "SodaStereo 08/05/2023",
+	//			"Las Pastillas del Abuelo 08/10/2023", "Hermetica 12/05/2023", "Fito Paez 13/07/2023",
+	//			"Leon Gieco 08/05/2023", "Sumo 24/10/2023" };
 
-		opConcierto = (String) JOptionPane.showInputDialog(null // para que se muestre centrado
-				, "Selecciona un Concierto" // Mensaje de la ventana
-				, "Ventasaurus - Conciertos" // Titulo de la ventana
-				, JOptionPane.QUESTION_MESSAGE // Icono
-				, icon // null para icono defecto de la ventana
-				, opcionesConcierto // el objeto
-				, opcionesConcierto[0] // posicion del que va aparecer seleccionado
-		);
-		if (opConcierto != null) {
-			op = JOptionPane.showOptionDialog(null, opConcierto + " \nLa aclamada banda hara su gira de despedida"
-					+ " \na lo grande, realizando un recorrido por sus"
-					+ " \ngrandes exitos ¿Que estas esperando? Saca tu" + " \nentrada." + " \nPrecios desde $1200",
-					"Ventasaurus - Administracion", 0, 0, null, opcClienteConc, 0);
-
-			switch (op) {
-			case 0:
-				CompraryPagar(opConcierto);
-				break;
-			case 1:
-				listaConciertosCliente();
-				break;
-			default:
-				break;
-			}
+//si la lista esta vacia decir que no hay conciertos
+//revisar
+		if (conciertoLista.length==0) {
+		JOptionPane.showMessageDialog(null, "No hay conciertos desponibles en este momento\nIntentelo mas tarde y/o comuniquese con un administrador",":(", JOptionPane.DEFAULT_OPTION,
+			new ImageIcon("src/img/troste.jpg"));
+} else if(conciertoLista.length!=0){
+	opConcierto = (String) JOptionPane.showInputDialog(
+			null // para que se muestre centrado
+			,"Selecciona un Concierto" // Mensaje de la ventana
+			,"Ventasaurus - Conciertos" // Titulo de la ventana
+			,JOptionPane.QUESTION_MESSAGE // Icono
+			,icon //null para icono defecto de la ventana
+			,conciertoLista// el objeto
+			,conciertoLista[0] // posicion del que va aparecer seleccionado
+			);
+	
+	if (opConcierto != null) {
+		op = JOptionPane.showOptionDialog(null, opConcierto + " \nLa aclamada banda hara su gira de despedida"
+				+ " \na lo grande, realizando un recorrido por sus"
+				+ " \ngrandes exitos ¿Que estas esperando? Saca tu" + " \nentrada." + " \nPrecios desde $1200",
+				"Ventasaurus - Administracion", 0, 0, null, opcClienteConc, 0);
+		switch (op) {
+		case 0:
+			CompraryPagar(opConcierto);
+			break;
+		case 1:
+			listaConciertosCliente();
+			break;
+		default:
+			break;
 		}
+	}
+
+}
+		
 	}
 
 	public static void SolicitudDeDevolucion() {
 		String opDevolucion;
 		//
 		/*
-		 
-		LinkedList<Concierto> listaTraida = verifica.verificaListaConciertos();
-	
-		String[] conciertoLista = new String[listaTraida.size()+1];
-		conciertoLista[0] = "Crear nuevo Concierto";
-		
-		i=1;
-		
-		for (Concierto concierto : listaTraida) {
-			conciertoLista[i] = concierto.getNombre();
-			i++;
-		}
-		
-		opConcierto = (String) JOptionPane.showInputDialog(
-				null // para que se muestre centrado
-				,"Selecciona un Concierto" // Mensaje de la ventana
-				,"Ventasaurus - Administracion" // Titulo de la ventana
-				,JOptionPane.QUESTION_MESSAGE // Icono
-				,null //null para icono defecto de la ventana
-				,conciertoLista // el objeto
-				,conciertoLista[0] // posicion del que va aparecer seleccionado
-				); 
-		 
-		 
+		 * 
+		 * LinkedList<Concierto> listaTraida = verifica.verificaListaConciertos();
+		 * 
+		 * String[] conciertoLista = new String[listaTraida.size()+1]; conciertoLista[0]
+		 * = "Crear nuevo Concierto";
+		 * 
+		 * i=1;
+		 * 
+		 * for (Concierto concierto : listaTraida) { conciertoLista[i] =
+		 * concierto.getNombre(); i++; }
+		 * 
+		 * opConcierto = (String) JOptionPane.showInputDialog( null // para que se
+		 * muestre centrado ,"Selecciona un Concierto" // Mensaje de la ventana
+		 * ,"Ventasaurus - Administracion" // Titulo de la ventana
+		 * ,JOptionPane.QUESTION_MESSAGE // Icono ,null //null para icono defecto de la
+		 * ventana ,conciertoLista // el objeto ,conciertoLista[0] // posicion del que
+		 * va aparecer seleccionado );
+		 * 
+		 * 
 		 */
+		
+		
 		String[] opcionesDev = { "Los Redondos 03/05/2023  x2 ", "SodaStereo 08/05/2023  x1",
 				"Las Pastillas del Abuelo 08/10/2023  x3", "Hermetica 12/05/2023  x2", "Fito Paez 13/07/2023  x4",
 				"Leon Gieco 08/05/2023  x2", "Sumo 24/10/2023 x1" };
@@ -109,33 +127,32 @@ public class MenuCliente {
 		} while (dni == null || dni.length() <= 7);
 
 		// habria q optimizar esta parte (tratar d hacerlo sin el try catch)
-		//try {
-			opDevolucion = (String) JOptionPane.showInputDialog(null // para que se muestre centrado
-					, "Selecciona un Concierto para generar una devolucion" // Mensaje de la ventana
-					, "Ventasaurus - Devoluciones" // Titulo de la ventana
-					, JOptionPane.QUESTION_MESSAGE // Icono
-					, icon // null para icono defecto de la ventana
-					, opcionesDev // el objeto
-					, opcionesDev[0] // posicion del que va aparecer seleccionado
-			);
-			// no toma el else?
-			if (opDevolucion != null && !opDevolucion.isEmpty()) {
-				JOptionPane.showMessageDialog(null,
-						"Solicitud recibida exitosamente" + "\nRecibirá un mail cuando la devolución se apruebe",
-						"Recibido", 1, null);
-			}else {
-				JOptionPane.showMessageDialog(null,
-						"No se eligio ningun concierto / Ocurrio un error al elegir el concierto"
-								+ "\nVolviendo al menú principal",
-						"Error inesperado", 0, null);
-			}
-			
-			// el catch lo soluciona
-		/*
-		} catch (Exception e) {
+		// try {
+		opDevolucion = (String) JOptionPane.showInputDialog(null // para que se muestre centrado
+				, "Selecciona un Concierto para generar una devolucion" // Mensaje de la ventana
+				, "Ventasaurus - Devoluciones" // Titulo de la ventana
+				, JOptionPane.QUESTION_MESSAGE // Icono
+				, icon // null para icono defecto de la ventana
+				, opcionesDev // el objeto
+				, opcionesDev[0] // posicion del que va aparecer seleccionado
+		);
+		// no toma el else?
+		if (opDevolucion != null && !opDevolucion.isEmpty()) {
 			JOptionPane.showMessageDialog(null,
-					"Ocurrio el siguiente error al tratar de generar una devolucion: \n" + e);
-		}*/
+					"Solicitud recibida exitosamente" + "\nRecibirá un mail cuando la devolución se apruebe",
+					"Recibido", 1, null);
+		} else {
+			JOptionPane.showMessageDialog(null,
+					"No se eligio ningun concierto / Ocurrio un error al elegir el concierto"
+							+ "\nVolviendo al menú principal",
+					"Error inesperado", 0, null);
+		}
+
+		// el catch lo soluciona
+		/*
+		 * } catch (Exception e) { JOptionPane.showMessageDialog(null,
+		 * "Ocurrio el siguiente error al tratar de generar una devolucion: \n" + e); }
+		 */
 
 		// guardarDevolucion();
 
@@ -146,7 +163,7 @@ public class MenuCliente {
 
 	public static void guardarDevolucion() { // falta la validacion y agregarlo a la base
 		/*
-		 * no va :( 
+		 * no va :(
 		 * 
 		 * Conexion con = new Conexion();
 		 * 
@@ -178,7 +195,7 @@ public class MenuCliente {
 		 * }
 		 */
 	}
-	
+
 // se entrga los tickets al cliente
 // averiguar valor 
 	public static void CompraryPagar(String nombreConcierto) {
@@ -186,18 +203,19 @@ public class MenuCliente {
 		ImageIcon icon = new ImageIcon("src/img/pago.png");
 		try {
 			cantEntradas = Integer
-					.parseInt(JOptionPane.showInputDialog(null, "Ingresar cantidad de tickets a comprar",""));
+					.parseInt(JOptionPane.showInputDialog(null, "Ingresar cantidad de tickets a comprar", ""));
 		} catch (Exception e) {
 			JOptionPane.showMessageDialog(null, "Ocurrio el siguiente error al tratar de comprar las entradas:\n" + e);
 			JOptionPane.showMessageDialog(null, "No pudiste compraste entradas");
 		}
 		if (cantEntradas > 0) {
-			JOptionPane.showMessageDialog(null, "Compraste " + cantEntradas + " entrada/s para : " + nombreConcierto
-					+ "\nPrecio final de la compra : 'x'","Gracias por comprar en Ventasaurus"/* precio sacado de la base de datos */, 0, icon);
-		}else {
+			JOptionPane.showMessageDialog(null,
+					"Compraste " + cantEntradas + " entrada/s para : " + nombreConcierto
+							+ "\nPrecio final de la compra : 'x'",
+					"Gracias por comprar en Ventasaurus"/* precio sacado de la base de datos */, 0, icon);
+		} else {
 			JOptionPane.showMessageDialog(null, "No compraste entradas");
 		}
-
 		listaConciertosCliente();
 	}
 
