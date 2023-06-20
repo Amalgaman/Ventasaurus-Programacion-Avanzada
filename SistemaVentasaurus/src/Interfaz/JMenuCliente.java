1b0d7d426143a9042382455270c86e9f89a40270
IgnacioPat <ignacio.patafio@davinci.edu.ar>
2023-06-19 23:01:31 -0300

    mas jframe
    ahora andando

--- SistemaVentasaurus/src/Interfaz/JMenuCliente.java
+++ SistemaVentasaurus/src/Interfaz/JMenuCliente.java
@@ -5,6 +5,14 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.JLabel;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import java.awt.Font;
+import javax.swing.JList;
+import javax.swing.JComboBox;
 
 public class JMenuCliente extends JFrame {
 
@@ -31,11 +39,35 @@ public class JMenuCliente extends JFrame {
 	 */
 	public JMenuCliente() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 700, 525);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JLabel imgConcierto = new JLabel();
+		 ImageIcon icon = new ImageIcon("src/img/dinosrock.png");
+		 imgConcierto.setIcon(icon);
+		imgConcierto.setBounds(33, 34, 454, 391);
+		contentPane.add(imgConcierto);
+		
+		JButton comprarPagar = new JButton("Elegir");
+		comprarPagar.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		comprarPagar.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+			}
+		});
+		comprarPagar.setBounds(539, 387, 100, 23);
+		contentPane.add(comprarPagar);
+		
+		JButton volverMenuPrincipal = new JButton("Volver atras");
+		volverMenuPrincipal.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		volverMenuPrincipal.setBounds(528, 439, 123, 23);
+		contentPane.add(volverMenuPrincipal);
+		
+		JComboBox listaConciertos = new JComboBox();
+		listaConciertos.setBounds(497, 34, 166, 29);
+		contentPane.add(listaConciertos);
 	}
-
 }
--- SistemaVentasaurus/src/Interfaz/JMenuPrincipal.java
+++ SistemaVentasaurus/src/Interfaz/JMenuPrincipal.java
@@ -5,6 +5,12 @@ import java.awt.EventQueue;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.swing.ImageIcon;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JLabel;
+import javax.swing.JTextArea;
+import javax.swing.UIManager;
 
 public class JMenuPrincipal extends JFrame {
 
@@ -31,11 +37,45 @@ public class JMenuPrincipal extends JFrame {
 	 */
 	public JMenuPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
-		setBounds(100, 100, 450, 300);
+		setBounds(100, 100, 750, 600);
 		contentPane = new JPanel();
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
 
 		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton verConciertos = new JButton("Ver conciertos disponibles");
+		verConciertos.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		verConciertos.setBounds(10, 494, 226, 43);
+		contentPane.add(verConciertos);
+		
+		JButton Devoluciones = new JButton("Solicitar devolución");
+		Devoluciones.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		Devoluciones.setBounds(244, 494, 177, 43);
+		contentPane.add(Devoluciones);
+		
+		JButton login = new JButton("Ingresar al sistema");
+		login.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		login.setBounds(431, 494, 195, 43);
+		contentPane.add(login);
+		
+		JButton salirBoton = new JButton("Salir");
+		salirBoton.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		salirBoton.setBounds(636, 504, 89, 23);
+		contentPane.add(salirBoton);
+		
+		JLabel imgDino = new JLabel();
+		 ImageIcon icon = new ImageIcon("src/img/dino.jpg");
+		 imgDino.setIcon(icon);
+		imgDino.setBounds(200, 54, 350, 412);
+		contentPane.add(imgDino);
+		
+		JTextArea bienvenida = new JTextArea();
+		bienvenida.setEditable(false);
+		bienvenida.setBackground(UIManager.getColor("CheckBox.background"));
+		bienvenida.setFont(new Font("Comic Sans MS", Font.BOLD, 20));
+		bienvenida.setText("Bienvenido a Ventasaurus");
+		bienvenida.setBounds(235, 0, 250, 43);
+		contentPane.add(bienvenida);
 	}
-
 }
