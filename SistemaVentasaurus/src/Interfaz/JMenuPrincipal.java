8b048824cd85005aae28f50569bc111318794c2c
IgnacioPat <ignacio.patafio@davinci.edu.ar>
2023-06-30 22:29:43 -0300

    nuevo tutorial

--- SistemaVentasaurus/src/Interfaz/JMenuPrincipal.java
+++ SistemaVentasaurus/src/Interfaz/JMenuPrincipal.java
@@ -40,7 +40,7 @@ public class JMenuPrincipal extends JFrame {
 	public JMenuPrincipal() {
 		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		setUndecorated(true);
-		setBounds(100, 100, 900, 400);
+		setBounds(100, 100, 900, 700);
 		contentPane = new JPanel();
 		contentPane.setBackground(Color.LIGHT_GRAY);
 		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
@@ -59,12 +59,12 @@ public class JMenuPrincipal extends JFrame {
 		});
 		
 		verConciertos.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
-		verConciertos.setBounds(50, 70, 206, 43);
+		verConciertos.setBounds(561, 80, 250, 43);
 		contentPane.add(verConciertos);
 		
 		JButton Devoluciones = new JButton("Solicitar devolución");
 		Devoluciones.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
-		Devoluciones.setBounds(346, 70, 211, 43);
+		Devoluciones.setBounds(135, 370, 250, 43);
 		contentPane.add(Devoluciones);
 		Devoluciones.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -78,7 +78,7 @@ public class JMenuPrincipal extends JFrame {
 		
 		JButton login = new JButton("Ingresar al sistema");
 		login.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
-		login.setBounds(657, 70, 182, 43);
+		login.setBounds(561, 370, 250, 43);
 		contentPane.add(login);
 		login.addActionListener(new ActionListener() {
 			public void actionPerformed(ActionEvent e) {
@@ -92,7 +92,7 @@ public class JMenuPrincipal extends JFrame {
 		
 		JButton salirBoton = new JButton("Salir");
 		salirBoton.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
-		salirBoton.setBounds(411, 349, 89, 23);
+		salirBoton.setBounds(410, 615, 89, 23);
 		ImageIcon icon = new ImageIcon("src/img/chau.gif");
 //		salirBoton.setIcon(icon);
 		contentPane.add(salirBoton);
@@ -113,7 +113,7 @@ public class JMenuPrincipal extends JFrame {
 		contentPane.add(bienvenida);
 		
 		JLabel conciertoImg = new JLabel("");
-		conciertoImg.setBounds(76, 136, 206, 145);
+		conciertoImg.setBounds(605, 157, 206, 145);
 		ImageIcon iconCon = new ImageIcon("src/img/compra.jpg");
 		conciertoImg.setIcon(iconCon);
 		contentPane.add(conciertoImg);
@@ -128,7 +128,7 @@ public class JMenuPrincipal extends JFrame {
 	
 		
 		JLabel devolucionImg = new JLabel("");
-		devolucionImg.setBounds(352, 136, 211, 138);
+		devolucionImg.setBounds(162, 442, 211, 138);
 		ImageIcon iconDev = new ImageIcon("src/img/devolucion.jpg");
 		devolucionImg.setIcon(iconDev);
 		contentPane.add(devolucionImg);
@@ -141,15 +141,42 @@ public class JMenuPrincipal extends JFrame {
 	        });
 		
 		JLabel loginImg = new JLabel("");
-		loginImg.setBounds(674, 135, 164, 145);
+		loginImg.setBounds(605, 443, 194, 163);
 		ImageIcon iconLog = new ImageIcon("src/img/admin.jpg");
 		loginImg.setIcon(iconLog);
 		contentPane.add(loginImg);
 		loginImg.addMouseListener(new MouseAdapter() {
             public void mouseClicked(MouseEvent e) {
-            	//Hacer login
+            	//Hacer devolucion
 				//dispose();
             	JOptionPane.showMessageDialog(null, "En proceso");
+            }
+        });
+		
+		JButton verTuto = new JButton("Tutorial: como sacar entradas");
+		verTuto.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Jtutorial nuevo = new Jtutorial();
+				nuevo.setLocationRelativeTo(null);
+				nuevo.setVisible(true);
+				dispose();
+			}
+		});
+		verTuto.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		verTuto.setBounds(135, 80, 250, 43);
+		contentPane.add(verTuto);
+		
+		JLabel tutorialImg = new JLabel("");
+		tutorialImg.setBounds(179, 134, 194, 210);
+		contentPane.add(tutorialImg);
+		ImageIcon iconTut = new ImageIcon("src/img/tutorial.jpeg");
+		tutorialImg.setIcon(iconTut);
+		tutorialImg.addMouseListener(new MouseAdapter() {
+            public void mouseClicked(MouseEvent e) {
+            	Jtutorial nuevo = new Jtutorial();
+				nuevo.setLocationRelativeTo(null);
+				nuevo.setVisible(true);
+				dispose();
             }
         });
 		
---
+++ SistemaVentasaurus/src/Interfaz/Jtutorial.java
@@ -0,0 +1,210 @@
+package Interfaz;
+
+import java.awt.EventQueue;
+
+import javax.swing.JFrame;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.border.EmptyBorder;
+import java.awt.Font;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.Color;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JTextArea;
+import javax.swing.Timer;
+import javax.swing.JCheckBox;
+
+public class Jtutorial extends JFrame {
+
+	private JPanel contentPane;
+
+	/**
+	 * Launch the application.
+	 */
+	public static void main(String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			public void run() {
+				try {
+					Jtutorial frame = new Jtutorial();
+					frame.setLocationRelativeTo(null);
+					frame.setVisible(true);
+				} catch (Exception e) {
+					e.printStackTrace();
+				}
+			}
+		});
+	}
+
+	/**
+	 * Create the frame.
+	 */
+	public Jtutorial() {
+		setResizable(false);
+		setUndecorated(true);
+		
+		setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		setBounds(100, 100, 670, 440);
+		contentPane = new JPanel();
+		contentPane.setBackground(Color.LIGHT_GRAY);
+		contentPane.setBorder(new EmptyBorder(5, 5, 5, 5));
+
+		setContentPane(contentPane);
+		contentPane.setLayout(null);
+		
+		JButton volverMenuPrincipal = new JButton("Volver atras");
+		volverMenuPrincipal.setBounds(537, 406, 123, 23);
+		volverMenuPrincipal.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		contentPane.add(volverMenuPrincipal);
+		volverMenuPrincipal.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				volverAlMenuPrincipalOg();
+			}
+		});
+		
+		JTextArea txtrPaso1 = new JTextArea();
+		txtrPaso1.setText("Paso 1:  Ingresar su numero de dni (sin puntos)");
+		txtrPaso1.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrPaso1.setEditable(false);
+		txtrPaso1.setBackground(Color.LIGHT_GRAY);
+		txtrPaso1.setBounds(10, 11, 412, 35);
+		contentPane.add(txtrPaso1);
+		
+		JTextArea txtrIngreseSuDni = new JTextArea();
+		txtrIngreseSuDni.setText("Ingrese su dni");
+		txtrIngreseSuDni.setFont(new Font("Comic Sans MS", Font.BOLD, 14));
+		txtrIngreseSuDni.setEditable(false);
+		txtrIngreseSuDni.setBackground(Color.LIGHT_GRAY);
+		txtrIngreseSuDni.setBounds(460, 11, 102, 23);
+		contentPane.add(txtrIngreseSuDni);
+		
+		JTextArea dni = new JTextArea();
+		dni.setBounds(448, 34, 123, 16);
+		contentPane.add(dni);
+		
+		JButton btnComprobarDni = new JButton("Comprobar dni");
+		btnComprobarDni.setFont(new Font("Comic Sans MS", Font.BOLD, 12));
+		btnComprobarDni.setBounds(448, 55, 123, 23);
+		contentPane.add(btnComprobarDni);
+		
+		JCheckBox soloDisponibles = new JCheckBox("Ver solo los conciertos disponibles");
+		soloDisponibles.setFont(new Font("Comic Sans MS", Font.BOLD, 11));
+		soloDisponibles.setBounds(149, 66, 207, 34);
+		contentPane.add(soloDisponibles);
+		
+		JTextArea txtrPaso2 = new JTextArea();
+		txtrPaso2.setText("Paso 2: Clikear ");
+		txtrPaso2.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrPaso2.setEditable(false);
+		txtrPaso2.setBackground(Color.LIGHT_GRAY);
+		txtrPaso2.setBounds(10, 68, 477, 35);
+		contentPane.add(txtrPaso2);
+		
+		JTextArea txtrPaso3 = new JTextArea();
+		txtrPaso3.setText("Paso 3: Tocar el boton continuar para pasar a la siguiente pestaña");
+		txtrPaso3.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrPaso3.setEditable(false);
+		txtrPaso3.setBackground(Color.LIGHT_GRAY);
+		txtrPaso3.setBounds(10, 164, 528, 23);
+		contentPane.add(txtrPaso3);
+		
+		JButton continuarBoton1 = new JButton("Continuar");
+		continuarBoton1.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		continuarBoton1.setBounds(179, 189, 102, 23);
+		contentPane.add(continuarBoton1);
+		
+		JTextArea txtrParaVerLos = new JTextArea();
+		txtrParaVerLos.setText("para ver los conciertos disponibles en ese momento y seleccionar uno de ellos");
+		txtrParaVerLos.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrParaVerLos.setEditable(false);
+		txtrParaVerLos.setBackground(Color.LIGHT_GRAY);
+		txtrParaVerLos.setBounds(20, 107, 640, 35);
+		contentPane.add(txtrParaVerLos);
+		
+		JTextArea txtrPaso4 = new JTextArea();
+		txtrPaso4.setText("Paso 4: Seleccionar una de las ubicaciones disponibles");
+		txtrPaso4.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrPaso4.setEditable(false);
+		txtrPaso4.setBackground(Color.LIGHT_GRAY);
+		txtrPaso4.setBounds(10, 223, 428, 35);
+		contentPane.add(txtrPaso4);
+		
+		JTextArea txtrPaso5 = new JTextArea();
+		txtrPaso5.setText("Paso 5: Escribir la cantidad de entradas a comprar (Minimo 1, Maximo 10)");
+		txtrPaso5.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrPaso5.setEditable(false);
+		txtrPaso5.setBackground(Color.LIGHT_GRAY);
+		txtrPaso5.setBounds(10, 287, 605, 35);
+		contentPane.add(txtrPaso5);
+		
+		JTextArea txtrPaso6 = new JTextArea();
+		txtrPaso6.setText("Paso 6: Tocar el boton continuar para comprar las entradas");
+		txtrPaso6.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		txtrPaso6.setEditable(false);
+		txtrPaso6.setBackground(Color.LIGHT_GRAY);
+		txtrPaso6.setBounds(10, 353, 528, 25);
+		contentPane.add(txtrPaso6);
+		
+		JButton continuarBoton2 = new JButton("Continuar");
+		continuarBoton2.setFont(new Font("Comic Sans MS", Font.BOLD, 13));
+		continuarBoton2.setBounds(179, 383, 102, 23);
+		contentPane.add(continuarBoton2);
+		btnComprobarDni.addActionListener(new ActionListener() {
+		public void actionPerformed(ActionEvent e) {
+			String dniText = dni.getText();
+			for (int i = 0; i < dniText.length(); i++) {
+		        if (Character.isDigit(dniText.charAt(i))&&(dniText.length() == 8)) {
+		            mostrarMensaje("El DNI es válido");
+		        } else if ((dniText.length() != 8)) {
+					mostrarMensaje("El DNI debe tener 8 caracteres");
+				} else {
+					mostrarMensaje("El DNI no es válido");
+				}   
+		        
+		    }}
+		});
+		
+		
+		
+		
+		
+}
+	private void mostrarMensaje(String mensaje) {
+		JDialog dialogo = new JDialog();
+		dialogo.setUndecorated(true);
+		// Establecer el tamaño del diálogo en función del mensaje
+		int ancho = 400;
+		int alto = 100 + (mensaje.length() / 30) * 20; // Ajusta el alto según la longitud del mensaje
+
+		dialogo.setSize(ancho, alto);
+
+		JLabel etiqueta = new JLabel(mensaje);
+		etiqueta.setFont(new Font("Comic Sans MS", Font.BOLD, 16));
+		dialogo.setLocationRelativeTo(null);
+
+		dialogo.getContentPane().add(etiqueta);
+
+		int duracionMilisegundos = 3000;
+		Timer temporizador = new Timer(duracionMilisegundos, new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				dialogo.dispose();
+			}
+		});
+		temporizador.setRepeats(false);
+		temporizador.start();
+
+		dialogo.setVisible(true);
+	}
+
+
+	public void volverAlMenuPrincipalOg() {
+		JMenuPrincipal nuevo = new JMenuPrincipal();
+		nuevo.setLocationRelativeTo(null);
+		nuevo.setVisible(true);
+		dispose();
+	}
+}
+
